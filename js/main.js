f45dd3717cfde41d462aa839262530f2d3459019
Valentin <valentinromero747@gmail.com>
2022-02-07 10:06:38 +0100

    Version Estable

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ const nuevaLetra = document.querySelector('#nueva-letra');
 const letrasJugador = document.querySelector('#letras-jugador');
 const mensajeGanado = document.querySelector('#mensaje-ganado');
 const mensajePerdido = document.querySelector('#mensaje-perdido');
+const numeroIntentos = document.querySelector('#numero-intentos');
 const visualUsuario = Array(solucion.length).fill('_');
 let intentos = 6;
 let letrasIntroducidas = [];
@@ -16,26 +17,52 @@ let letrasIntroducidas = [];
  * Funciones
  */
 
+/**
+ * Genera, a partir de la solucion, el string que vera el usuario:
+ * Ejemplo: _r__a
+ * @param {string} solucionLocal - Solucion a adivinar.
+ * @param {Array} letrasUsuarioLocal - Listado de letras introducidas por el usuario.
+ * @param {string} vacio - Simbolo que oculta la letra en caso de no ser adivinada a partir de 'letrasUsuarioLocal'.
+ * @return {Array}
+ */
+function generarEnigmaConGuiones(solucionLocal, letrasUsuarioLocal, vacio) {
+    return solucionLocal.split("").map(function (letra) {
+        return letrasUsuarioLocal.includes(letra) ? letra : vacio;
+    })
+}
 
 /**
  * Renderiza el estado de la palabra a adivinar.
  * @param {string} solucionLocal - Palabra a adivinar.
  * @param {Array} letrasUsuario - Letras introducidas por el usuario.
  * @param {HTMLElement} targetAdivinadas - Lugar donde se mostrara el estado de la adinivinacion.
+ * @param {number} numeroIntentosLocal - Numero de intentos restantes
+ * @param {HTMLElement} targetNumeroIntentos - Lugar donde se mostrara los numeros de intentos.
+ * @param {HTMLElement} targetLetrasJugador - Lugar donde se mostrara las letras del jugador que ha introducido.
  * @param {string} vacio - Caracter de letra aun no adivinadaa. Por defecto "_".
  * @param {string} separador - Separacion entre los caracteres mostrados. Por defecto " ".
  */
-function render(solucionLocal, letrasUsuario, targetAdivinadas, vacio="_", separador=" ") {
+function render(solucionLocal,
+                letrasUsuario,
+                targetAdivinadas,
+                numeroIntentosLocal,
+                targetNumeroIntentos,
+                targetLetrasJugador,
+                vacio="_",
+                separador=" " ) {
     // Letra y guiones
-    const finalHTML = solucionLocal.split('').map(function (letra) {
-        return letrasUsuario.includes(letra) ? letra : vacio;
-    });
+    const finalHTML = generarEnigmaConGuiones(solucionLocal, letrasUsuario, vacio);
+    // Numero de intentos
+    targetNumeroIntentos.textContent = numeroIntentosLocal;
+    // Letras introducidas por el usuario
+    targetLetrasJugador.textContent = letrasIntroducidas.join(separador);
     // Imprimimos
     targetAdivinadas.textContent = finalHTML.join(separador);
 }
 
 /**
  * Anyade la letra del usuario
+ * @return {string} Letra del usuario
  */
 function anyadirLetra() {
     // Recogemos letra
@@ -44,8 +71,7 @@ function anyadirLetra() {
     letrasIntroducidas.push(miNuevaLetra);
     // Limpiamos la letra introducida
     nuevaLetra.value = '';
-    // Renderizamos los cambios
-    render(solucion, letrasIntroducidas, visualJugador);
+    return miNuevaLetra;
 }
 
 /**
@@ -71,12 +97,55 @@ function restarPuntos(intentos, puntosARestar = 1) {
 }
 
 /**
+ * Comprueba si ha fallado el usuario y resta un intento
+ * @param {string} solucion - Palabra a adivinar.
+ * @param {string} letraLocal - Nueva letra introducida por el usuario.
+ * @return {number} Numero intentos
+ */
+function comprobarLetraAcertada(solucion, letraLocal) {
+    // Ha fallado
+    if(!incluyeCaracter(solucion, letraLocal)) {
+        intentos = restarPuntos(intentos);
+    }
+    return intentos;
+}
+
+/**
+ * Informa al usuario si ha ganado o ha perdido. Bloquea el input en cualquier caso.
+ * @param {string} solucion
+ * @param {Array} letrasIntroducidas
+ * @param {number} intentos
+ * @param {HTMLElement} mensajeGanado
+ * @param {HTMLElement} mensajePerdido
+ * @param {HTMLElement} nuevaLetra - Input donde el usuario indica la nueva letra.
+ * @param {string} claseMostrar - Clase que añadiremos a 'mensajeGanado' o 'mensajePerdido'.
+ */
+function informarGameOver(solucion, letrasIntroducidas, intentos, mensajeGanado, mensajePerdido, nuevaLetra, claseMostrar="mensaje--show") {
+    // Ha perdido
+    if (intentos === 0) {
+        mensajePerdido.classList.add(claseMostrar);
+        // Bloquea la posibilidad de añadir nuevos elementos
+        nuevaLetra.setAttribute('disabled', true);
+    }
+    // Ha ganado
+    const visualUsuario = generarEnigmaConGuiones(solucion, letrasIntroducidas, '').join('');
+    if (solucion === visualUsuario) {
+        mensajeGanado.classList.add(claseMostrar);
+        // Bloquea la posibilidad de añadir nuevos elementos
+        nuevaLetra.setAttribute('disabled', true);
+    }
+}
+
+/**
  * Eventos
  */
-nuevaLetra.addEventListener('keyup', function(event) {
+nuevaLetra.addEventListener('keyup', function (event) {
+    event.preventDefault();
     if (event.key === "Enter") {
-        anyadirLetra();
-        render(solucion, letrasIntroducidas, visualJugador);
+        const nuevaLetraIntroducida = anyadirLetra();
+        comprobarLetraAcertada(solucion, nuevaLetraIntroducida);
+        informarGameOver(solucion, letrasIntroducidas, intentos, mensajeGanado, mensajePerdido, nuevaLetra);
+        render(solucion, letrasIntroducidas, visualJugador, intentos, numeroIntentos, letrasJugador);
     }
 })
 
@@ -84,4 +153,4 @@ nuevaLetra.addEventListener('keyup', function(event) {
  * Inicio
  */
 
-render(solucion, letrasIntroducidas, visualJugador);
+render(solucion, letrasIntroducidas, visualJugador, intentos, numeroIntentos, letrasJugador);
